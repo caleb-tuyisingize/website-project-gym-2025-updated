c1b8a0138a0b27ac35ccc887df157a4a93f286c1
Docile123 <imbereyemasoo@gmail.com>
2025-10-16 11:08:50 +0200

    Update the design

--- src/js/nav.js
+++ src/js/nav.js
@@ -14,7 +14,7 @@ function initNav() {
     const ci = document.getElementById('close-icon');
 
     mi.addEventListener('click', () => {
-        menu.classList.toggle('max-lg:hidden');
+        menu.classList.toggle('max-xl:hidden');
         mi.classList.toggle('hidden');
         ci.classList.toggle('hidden');
         console.log('menu opened');
@@ -22,7 +22,7 @@ function initNav() {
 
     // close and open the menu on the small screen
     ci.addEventListener('click', () => {
-        menu.classList.toggle('max-lg:hidden');
+        menu.classList.toggle('max-xl:hidden');
         mi.classList.toggle('hidden');
         ci.classList.toggle('hidden');
         console.log('menu closed');
